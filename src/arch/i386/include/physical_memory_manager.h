85698e8f0353c3e980b2337dba142d4e9915a145
Cole Whiteside <cbwhiteside50@gmail.com>
2024-03-02 21:23:50 -0600

    Fixed TODOs on physical_memory_manager.h

--- src/arch/i386/include/physical_memory_manager.h
+++ src/arch/i386/include/physical_memory_manager.h
@@ -10,14 +10,17 @@ static uint32_t *memory_map;
 #define BLOCK_SIZE          4096        // 4 Kb
 #define BLOCKS_PER_BYTE     8           // Each bit represents a block
 
-// These are declared as inline due to their amount of usage
+// These are declared as inline due to their high amount of usage
 // This increases memory speed at the cost of code size
+
+// TODO: Finish malloc function
 inline void malloc(uint32_t bit) {
     uint32_t free_block;
 
     free_block = find_free_block();
 }
 
+// TODO: Finish find_free_block function
 inline uint32_t find_free_block() {
     for (uint32_t bit; bit < memory_map[bit]; bit++) {
         if (memory_map[bit % 32] == 0x00) {
